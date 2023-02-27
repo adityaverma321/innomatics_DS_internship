129722d9ad5a4c5f98d8e8a1efab271cc9d597bc
Aditya Verma <59736486+adityaverma321@users.noreply.github.com>
2023-02-27 16:00:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.header("Hello World")
+st.title(":red[Innomatics] Data App")
+
+st.balloons()
+
+if st.button('Click Me!'):
+    st.subheader("Hello everyone, I am Aditya and this is my first Streamlit app.")    
